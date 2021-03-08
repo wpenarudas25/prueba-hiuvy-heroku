7b54437719e27f0946b6e4ebd1eeecaa90af98bd
Wilmar Peña Rudas <wpenarudas@gmail.com>
2021-03-08 10:51:25 -0500

    Actualizando el documento data.sql

--- data.sql
+++ data.sql
@@ -1,10 +1,6 @@
-INSERT INTO user (`email`,`first_name`, `last_name`, `password`, `username`) 
+INSERT INTO user (email, first_name, last_name, password, username)
 VALUES ('admin@admin.com','Wilmar', 'Peña', 'admin', 'admin');
-
-
-INSERT INTO role (`description`, `name`) VALUES ('ROLE_ADMIN', 'ADMIN');
-INSERT INTO role (`description`, `name`) VALUES ('ROLE_USER', 'USER');
-INSERT INTO role (`description`, `name`) VALUES ('ROLE_SUPERVISOR', 'SUPERVISOR');
-INSERT INTO role (`description`, `name`) VALUES ('ROLE_EDITOR', 'EDITOR');
-
-INSERT INTO user_roles (`user_id`, `role_id`) VALUES ('1', '1');
+INSERT INTO role (description, name) VALUES ('ROLE_ADMIN', 'ADMIN');
+INSERT INTO role (description, name) VALUES ('ROLE_USER', 'USER');
+INSERT INTO role (description, name) VALUES ('ROLE_SUPERVISOR', 'SUPERVISOR');
+INSERT INTO user_roles (user_id, role_id) VALUES ('1', '1');
